38aef9904f8ff22353f93ee92908b2a37791393f
Marwan Sarieddine <marwan@anyscale.com>
2024-06-11 18:46:49 -0700

    improve draft implementation

--- ai_data.py
+++ ai_data.py
@@ -1,18 +1,78 @@
 import pandas as pd
 from openai import OpenAI
+from typing import Any, Optional
 from pathlib import Path
-from sentence_transformers import SentenceTransformer
 import ray
 from openai import OpenAI
-import numpy as np
+from pinecone.grpc import PineconeGRPC as Pinecone
+from pinecone import ServerlessSpec
+import os
+import joblib
+# from tenacity import retry, stop_after_attempt
 
 
-def query_llava(base_url: str, api_key: str, text: str, image_url: str):
+def setup_pinecone() -> None:
+    pinecone_api_key = os.environ.get(
+        "PINECONE_API_KEY", "8bacffe0-949c-480e-af65-5b309b7c9fe4"
+    )
+    pc = Pinecone(api_key=pinecone_api_key)
+    pc.create_index(
+        name="mongodb-demo",
+        dimension=1024,
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+    )
+
+
+def convert_to_pinecone_vectors(row: dict[str, Any], col: str) -> dict[str, Any]:
+    return {
+        "id": row["name"],
+        "values": row[col],
+        "metadata": {}
+    }
+
+
+def upsert_pinecone(batch, namespace: str = "descriptions"):
+    pinecone_api_key = os.environ.get(
+        "PINECONE_API_KEY", "8bacffe0-949c-480e-af65-5b309b7c9fe4"
+    )
+    pc = Pinecone(api_key=pinecone_api_key)
+    index = pc.Index("mongodb-demo")
+    index.upsert(
+        vectors=[
+            {
+                "id": id_,
+                "values": values,
+                "metadata": metadata,
+            }
+            for id_, values, metadata in zip(
+                batch["id"], batch["values"], batch["metadata"]
+            )
+        ],
+        namespace=namespace,
+    )
+    return batch
+
+# @retry(stop=stop_after_attempt(3))
+def query_embedding(base_url: str, api_key: str, text: str) -> str:
     client = OpenAI(
         base_url=base_url,
         api_key=api_key,
     )
-    chat_completions = client.chat.completions.create(
+
+    response = client.embeddings.create(
+        input=text,
+        model="thenlper/gte-large",
+    )
+    return response.data[0].embedding
+
+# @retry(stop=stop_after_attempt(3))
+def query_llava(base_url: str, api_key: str, text: str, image_url: str) -> str:
+    client = OpenAI(
+        base_url=base_url,
+        api_key=api_key,
+    )
+    response = client.chat.completions.create(
         model="llava-hf/llava-v1.6-mistral-7b-hf",
         messages=[
             {
@@ -30,15 +90,15 @@ def query_llava(base_url: str, api_key: str, text: str, image_url: str):
         stream=False,
     )
 
-    return chat_completions
+    return str(response.choices[0].message.content)
 
-
-def query_llama(base_url: str, api_key: str, text: str):
+# @retry(stop=stop_after_attempt(3))
+def query_llama(base_url: str, api_key: str, text: str) -> str:
     client = OpenAI(
         base_url=base_url,
         api_key=api_key,
     )
-    chat_completions = client.chat.completions.create(
+    response = client.chat.completions.create(
         model="meta-llama/Llama-2-70b-chat-hf",
         messages=[
             {
@@ -50,81 +110,140 @@ def query_llama(base_url: str, api_key: str, text: str):
         stream=False,
     )
 
-    return chat_completions
+    return str(response.choices[0].message.content)
 
 
-def generate_description(text, image):
-    out = query_llava(
-        base_url="https://api.endpoints.anyscale.com/v1",
-        api_key="esecret_wvfv1x446u8ifxujuqkimm7wjw",
-        text=f"Generate an ecommerce product description given the image and this title: {text}.",
-        image_url=image,
-    )
-    return out.choices[0].message.content
+def generate_description(text: str, image: str) -> Optional[str]:
+    try:
+        out = query_llava(
+            base_url="https://api.endpoints.anyscale.com/v1",
+            api_key="esecret_wvfv1x446u8ifxujuqkimm7wjw",
+            text=f"Generate an ecommerce product description given the image and this title: {text}.",
+            image_url=image,
+        )
+    except Exception:
+        out = None
+    return out
 
 
-def generate_category(title, description):
+def generate_category(title: str, description: str) -> Optional[str]:
     categories = ["Tops", "Bottoms", "Dresses", "Footwear", "Accessories"]
     categories_str = ", ".join(categories)
-    out = query_llama(
-        base_url="https://api.endpoints.anyscale.com/v1",
-        api_key="esecret_wvfv1x446u8ifxujuqkimm7wjw",
-        text=(
-            f"Given the title of this product: {title} and "
-            f"the description: {description}, what category does it belong to? "
-            f"Chose from the following categories: {categories_str}. "
-            "Return the category that best fits the product. Only return the category name and nothing else."
-        ),
-    )
-    category = out.choices[0].message.content
+    try:
+        category = query_llama(
+            base_url="https://api.endpoints.anyscale.com/v1",
+            api_key="esecret_wvfv1x446u8ifxujuqkimm7wjw",
+            text=(
+                f"Given the title of this product: {title} and "
+                f"the description: {description}, what category does it belong to? "
+                f"Chose from the following categories: {categories_str}. "
+                "Return the category that best fits the product. Only return the category name and nothing else."
+            ),
+        )
+
+    except Exception:
+        return None
+
     if category in categories:
         return category
 
+    return None
 
-def update_record(row):
+def generate_embedding(text: str) -> Optional[list[float]]:
+    try:
+        out = query_embedding(
+            base_url="https://api.endpoints.anyscale.com/v1",
+            api_key="esecret_wvfv1x446u8ifxujuqkimm7wjw",
+            text=text,
+        )
+    except Exception:
+        out = None
+    return out
+
+
+def update_record(row: dict[str, Any]) -> dict[str, Any]:
     last_img = row["img"].split(";")[-1].strip()
-    description = generate_description(text=row["name"], image=last_img)
-    category = generate_category(title=row["name"], description=description)
+    name = row["name"]
+    name_embedding = generate_embedding(name)
+    
+    description = generate_description(text=name, image=last_img)
+    if description is not None:
+        category = generate_category(title=name, description=description)
+        description_embedding = generate_embedding(description)
+    else:
+        category = None
+        description_embedding = None
+
     return {
         **row,
         "description": description,
         "category": category,
+        "name_embedding": name_embedding,
+        "description_embedding": description_embedding,
     }
 
+def update_embeddings(row: dict[str, Any]) -> dict[str, Any]:
+    name = row["name"]
+    name_embedding = generate_embedding(name)
+    description = row["description"]
+    description_embedding = generate_embedding(description)
+    return {
+        **row,
+        "name_embedding": name_embedding,
+        "description_embedding": description_embedding,
+    }
 
-class Embedder:
-    def __init__(self) -> None:
-        self.model = SentenceTransformer("thenlper/gte-large", device="mps")
+# class Embedder:
+#     def __init__(self) -> None:
+#         self.model = SentenceTransformer("thenlper/gte-large", device="mps")
 
-    def __call__(self, batch: dict[str, np.ndarray]) -> np.ndarray:
-        texts = batch["description"].tolist()
-        batch["embedding"] = self.model.encode(texts, batch_size=len(texts))
-        return batch
+#     def __call__(self, batch: dict[str, np.ndarray]) -> np.ndarray:
+#         texts = batch["description"].tolist()
+#         batch["embedding"] = self.model.encode(texts, batch_size=len(texts))
+#         return batch
 
 
 if __name__ == "__main__":
-    # data = []
-    # for row in df.itertuples():
-    #     description = generate_description(row.name, row.img)
-    #     category = generate_category(row.name, description)
-    #     embedding = generate_embedding(row.name)
-    #     row_dict = row._asdict()
-    #     data.append(
-    #         {
-    #             **row_dict,
-    #             "description": description,
-    #             "category": category,
-    #             "embedding": embedding,
-    #         }
 
-    #     )
-    # data.to_csv("data_with_ai.csv")
-    df = pd.read_csv(Path(__file__).parent / "myntra202305041052.csv", nrows=100)
+    # df = (
+    #     pd.read_csv("/mnt/cluster_storage/myntra202305041052.csv")
+    #     .dropna(subset=["name", "purl", "img", "price", "rating"], how="any")
+    #     .drop_duplicates(subset=["name"])
+    #     .drop_duplicates(subset=["purl"])
+    #     .sample(n=1000)
+    # )
+
+
+    # ds = (
+    #     ray.data.from_pandas(df)
+    #     .repartition(1000)
+    #     .map(update_record, concurrency=24, num_cpus=0.01)
+    #     .repartition(2)
+    #     .write_json("/mnt/cluster_storage/data_with_ai_6")
+    # )
+    # setup_pinecone()
+
+    # ds = (
+    #     ray.data.read_json("/mnt/cluster_storage/data_with_ai_7")
+    #     .repartition(400)
+    #     .map(update_embeddings, concurrency=24, num_cpus=0.01)
+    #     .repartition(1)
+    #     .write_json("/mnt/cluster_storage/data_with_ai_8")
+    # )
+
+
+    # ds = (
+    #     ray.data.read_json("/mnt/cluster_storage/data_with_ai_8")
+    #     .repartition(400)
+    #     .map(convert_to_pinecone_vectors, concurrency=24, num_cpus=0.01, fn_kwargs={"col": "description_embedding"})
+    #     .map_batches(upsert_pinecone, concurrency=10, num_cpus=0.01)
+    #     .materialize()
+    # )
+
     ds = (
-        ray.data.from_pandas(df)
-        .map(update_record, concurrency=10, num_cpus=0.01)
-        # .map_batches(Embedder, batch_size=10, concurrency=4, num_cpus=1)
-        .write_json("data_with_ai_2")
+        ray.data.read_json("/mnt/cluster_storage/data_with_ai_8")
+        .repartition(400)
+        .map(convert_to_pinecone_vectors, concurrency=24, num_cpus=0.01, fn_kwargs={"col": "name_embedding"})
+        .map_batches(upsert_pinecone, concurrency=10, num_cpus=0.01, fn_kwargs=dict(namespace="names"))
+        .materialize()
     )
-
-# def compute_similarity(text1, text2): ...
--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import math
 import ray
 import gradio as gr
-import pandas as pd
-from gradio_rangeslider import RangeSlider
-from pathlib import Path
 import openai
-
-# df_no_ai = pd.read_csv(Path(__file__).parent / "myntra202305041052.csv", nrows=10)
-df = ray.data.read_json("data_with_ai").to_pandas()
-
+from ray.serve.gradio_integrations import GradioServer
+from functools import lru_cache
+from pinecone.grpc import PineconeGRPC as Pinecone
+import os
+from more_itertools import chunked
 
 def get_embedding(text):
     client = openai.OpenAI(
@@ -17,14 +15,58 @@ def get_embedding(text):
     )
 
     response = client.embeddings.create(
-        input=text, model="thenlper/gte-large", 
+        input=text,
+        model="thenlper/gte-large",
     )
     return response.data[0].embedding
 
+def query_vector_index(text: str, threshold: float):
+    retries = 3
+    while retries > 0:
+        try:
+            embedding = get_embedding(text)
+            print("got embedding")
+        except Exception as e:
+            print(f"Error: {e}")
+            embedding = None
+            retries -= 1
+            continue
+        else:
+            break
+    if embedding:
+        pinecone_api_key = os.environ.get(
+            "PINECONE_API_KEY", "8bacffe0-949c-480e-af65-5b309b7c9fe4"
+        )
+        pc = Pinecone(api_key=pinecone_api_key)
+        index = pc.Index("mongodb-demo")
+        result = index.query(
+            vector=embedding,
+            top_k=20,
+            namespace="descriptions",
+        )
+        print("queried pinecone")
+        matches = [
+            match for match in result["matches"]
+        ]
+        print(f"{matches=}")
+        filtered = [
+            match["id"] for match in result["matches"]
+            if match["score"] > threshold
+        ]
+        print(f"{filtered=}")
+        return filtered
+
+
+@lru_cache
+def read_data():
+    df = ray.data.read_json("s3://anyscale-public-materials/mongodb-demo/data_with_ai_v3/").to_pandas()
+    # df = ray.data.read_json("/mnt/cluster_storage/data_with_ai_8").to_pandas()
+    return df
+
 
 # Filter function
-def filter_products(keywords_str, price, min_rating):
-    min_price, max_price = price
+def filter_products(keywords_str, min_price, max_price, min_rating, n=200):
+    df = read_data()
     filtered_df = df[
         (df["price"] >= min_price)
         & (df["price"] <= max_price)
@@ -45,122 +87,151 @@ def filter_products(keywords_str, price, min_rating):
                 )
             )
         ]
-    return [
+    results =  [
         (row["img"].split(";")[-1].strip(), row["name"])
         for _, row in filtered_df.iterrows()
     ]
-    
+
+    return list(chunked(results, 10))
+
 
 def filter_products_with_ai(
-    synthetic_category, text_search, keywords_str, price, min_rating
+    synthetic_categories, text_search, min_price, max_price, min_rating, n=20
 ):
-    min_price, max_price = price
-    # text_embedding = get_embedding(text_search)
+    df = read_data()
+    
     filtered_df = df[
         (df["price"] >= min_price)
         & (df["price"] <= max_price)
         & (df["rating"] >= min_rating)
-        & (df["category"] == synthetic_category)
+        & (df["category"].isin(synthetic_categories))
     ]
-    if keywords_str.strip():
-        keywords = keywords_str.split(",")
-        # matching any of the keywords
-        # filtered_df = filtered_df[
-        #     filtered_df["name"].str.contains("|".join(keywords), case=False)
-        # ]
-        # matching all of the keywords
+
+    if text_search.strip():
+        closest_names_based_on_text_search = query_vector_index(text_search, threshold=0.8)
         filtered_df = filtered_df[
-            filtered_df["name"].apply(
-                lambda name: all(
-                    keyword.strip().lower() in name.lower().split()
-                    for keyword in keywords
-                )
-            )
+            filtered_df["name"].isin(closest_names_based_on_text_search)
         ]
+
+
     return [
         (row["img"].split(";")[-1].strip(), row["name"])
         for _, row in filtered_df.iterrows()
-    ]
+    ][:n]
 
 
-# Get min, max and round off to nearest 100
-price_min = df["price"].min()
-price_min = int(price_min - (price_min % 100))
-price_max = df["price"].max()
-price_max = int(price_max + (100 - price_max % 100))
+def build_interface():
+    df = read_data()
 
-# Get rating range
-rating_min = float(df["rating"].min())
-rating_min = int(rating_min)
-rating_max = float(df["rating"].max())
-rating_max = math.ceil(rating_max)
+    # Get min, max and round off to nearest 100
+    price_min = df["price"].min()
+    price_min = int(price_min - (price_min % 100))
+    price_max = df["price"].max()
+    price_max = int(price_max + (100 - price_max % 100))
 
-print(
-    f"Using price range: {price_min} - {price_max}"
-    f" and rating range: {rating_min} - {rating_max}"
-)
+    # Get rating range
+    rating_min = float(df["rating"].min())
+    rating_min = int(rating_min)
+    rating_max = float(df["rating"].max())
+    rating_max = math.ceil(rating_max)
 
-print(type(price_min), type(price_max), type(rating_min), type(rating_max))
+    print(
+        f"Using price range: {price_min} - {price_max}"
+        f" and rating range: {rating_min} - {rating_max}"
+    )
 
-# Gradio Interface
-with gr.Blocks(
-    # theme="shivi/calm_foam",
-    title="Multi-modal search",
-) as iface:
-    with gr.Tab(label="Legacy Search"):
-        with gr.Row():
-            with gr.Column(scale=1):
-                keywords_component = gr.Textbox(label="Keywords")
-                price_range_component = RangeSlider(
-                    price_min, price_max, label="Price Range"
-                )
-                min_rating_component = gr.Slider(
-                    rating_min, rating_max, step=0.25, label="Min Rating"
-                )
-                filter_button_component = gr.Button("Filter")
-            with gr.Column(scale=3):
-                gallery = gr.Gallery(label="Filtered Products", columns=3, height=800)
-        inputs = [keywords_component, price_range_component, min_rating_component]
-        filter_button_component.click(filter_products, inputs=inputs, outputs=gallery)
-        iface.load(
-            filter_products,
-            inputs=inputs,
-            outputs=gallery,
-        )
+    print(type(price_min), type(price_max), type(rating_min), type(rating_max))
 
-    with gr.Tab(label="AI enabled search"):
-        with gr.Row():
-            with gr.Column(scale=1):
-                synthetic_category_dropdown = gr.Dropdown(
-                    ["Tops", "Bottoms", "Dresses", "Footwear", "Accessories"],
-                    label="Category",
-                )
-                text_component = gr.Textbox(label="Text Search")
-                keywords_component = gr.Textbox(label="Keywords")
-                price_range_component = RangeSlider(
-                    price_min, price_max, label="Price Range"
-                )
-                min_rating_component = gr.Slider(
-                    rating_min, rating_max, step=0.25, label="Min Rating"
-                )
-                filter_button_component = gr.Button("Filter")
-            with gr.Column(scale=3):
-                gallery = gr.Gallery(label="Filtered Products", columns=3, height=800)
-        inputs = [
-            synthetic_category_dropdown,
-            text_component,
-            keywords_component,
-            price_range_component,
-            min_rating_component,
-        ]
-        filter_button_component.click(
-            filter_products_with_ai, inputs=inputs, outputs=gallery
-        )
-        iface.load(
-            filter_products_with_ai,
-            inputs=inputs,
-            outputs=gallery,
-        )
+    # Gradio Interface
+    with gr.Blocks(
+        # theme="shivi/calm_foam",
+        title="Multi-modal search",
+    ) as iface:
+        with gr.Tab(label="Legacy Search"):
+            with gr.Row():
+                with gr.Column(scale=1):
+                    keywords_component = gr.Textbox(label="Keywords")
+                    min_price_component = gr.Slider(
+                        price_min, price_max, label="Min Price", value=price_min
+                    )
+                    max_price_component = gr.Slider(
+                        price_min, price_max, label="Max Price", value=price_max
+                    )
+                    min_rating_component = gr.Slider(
+                        rating_min, rating_max, step=0.25, label="Min Rating"
+                    )
+                    filter_button_component = gr.Button("Filter")
+                with gr.Column(scale=3):
+                    gallery = gr.Gallery(
+                        label="Filtered Products", columns=3, height=800, examples_per_page=10
+                    )
+            inputs = [
+                keywords_component,
+                min_price_component,
+                max_price_component,
+                min_rating_component,
+            ]
+            filter_button_component.click(
+                filter_products, inputs=inputs, outputs=gallery
+            )
+            iface.load(
+                filter_products,
+                inputs=inputs,
+                outputs=gallery,
+            )
 
-# Launch the interface
-iface.launch()
+        with gr.Tab(label="AI enabled search"):
+            with gr.Row():
+                with gr.Column(scale=1):
+                    synthetic_category_component = gr.CheckboxGroup(
+                        ["Tops", "Bottoms", "Dresses", "Footwear", "Accessories"],
+                        label="Category",
+                        value=[
+                            "Tops",
+                            "Bottoms",
+                            "Dresses",
+                            "Footwear",
+                            "Accessories",
+                        ],
+                    )
+                    text_component = gr.Textbox(label="Text Search")
+                    min_price_component = gr.Slider(
+                        price_min, price_max, label="Min Price", value=price_min
+                    )
+                    max_price_component = gr.Slider(
+                        price_min, price_max, label="Max Price", value=price_max
+                    )
+
+                    min_rating_component = gr.Slider(
+                        rating_min, rating_max, step=0.25, label="Min Rating"
+                    )
+                    filter_button_component = gr.Button("Filter")
+                with gr.Column(scale=3):
+                    gallery = gr.Gallery(
+                        label="Filtered Products", columns=3, height=800, examples_per_page=10
+                    )
+            inputs = [
+                synthetic_category_component,
+                text_component,
+                min_price_component,
+                max_price_component,
+                min_rating_component,
+            ]
+            filter_button_component.click(
+                filter_products_with_ai, inputs=inputs, outputs=gallery
+            )
+            iface.load(
+                filter_products_with_ai,
+                inputs=inputs,
+                outputs=gallery,
+            )
+
+    # Launch the interface
+    # iface.launch()
+    # iface.queue(max_size=20)
+    return iface
+
+
+app = GradioServer.options(ray_actor_options={"num_cpus": 1}).bind(build_interface)
+# if __name__ == "__main__":
+#     iface = build_interface()
