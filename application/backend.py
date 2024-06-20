cede9be407ce60d65ddef2ba62b2e3fb7598704c
Marwan Sarieddine <sarieddine.marwan@gmail.com>
2024-06-20 18:09:06 +0300

    update text name defaults to match setup

--- application/backend.py
+++ application/backend.py
@@ -45,7 +45,7 @@ class QueryLegacySearch:
             pipeline.append(
                 {
                     "$search": {
-                        "index": "name-index-search",
+                        "index": "lexical_text_search_index",
                         "text": {
                             "query": text_search,
                             "path": "name",
@@ -97,160 +97,176 @@ class QueryWithVectorSearch:
         colors: list[str],
         seasons: list[str],
         n: int = 20,
-        vector_search_index_name: str = "vector_index",
+        vector_search_index_name: str = "vector_search_index",
         vector_search_path: str = "description_embedding",
-        text_search_index_name: str = "name-index-search",
+        text_search_index_name: str = "lexical_text_search_index",
         vector_penalty: int = 1,
         full_text_penalty: int = 10,
+        score_threshold: float = 0.3,
     ):
+        logger = logging.getLogger("ray.serve")
+        logger.setLevel(logging.DEBUG)
+
         db = self.client[self.database_name]
         collection = db[self.collection_name]
 
         if text_search.strip():
+
+            logger.debug(f"Computing embedding for {text_search=}")
             embedding = await self.embedding_model.compute_embedding.remote(text_search)
-            records = collection.aggregate(
-                [
-                    {
-                        "$vectorSearch": {
-                            "index": vector_search_index_name,
-                            "path": vector_search_path,
-                            "queryVector": embedding,
-                            "numCandidates": 100,
-                            "limit": 20,
-                            "filter": {
-                                "price": {"$gte": min_price, "$lte": max_price},
-                                "rating": {"$gte": min_rating},
-                                "category": {"$in": synthetic_categories},
-                                "color": {"$in": colors},
-                                "season": {"$in": seasons},
-                            },
-                        }
-                    },
-                    {"$group": {"_id": None, "docs": {"$push": "$$ROOT"}}},
-                    {"$unwind": {"path": "$docs", "includeArrayIndex": "rank"}},
-                    {
-                        "$addFields": {
-                            "vs_score": {
-                                "$divide": [1.0, {"$add": ["$rank", vector_penalty, 1]}]
-                            }
-                        }
-                    },
-                    {
-                        "$project": {
-                            "vs_score": 1,
-                            "_id": "$docs._id",
-                            "name": "$docs.name",
-                            "img": "$docs.img",
-                        }
-                    },
-                    {
-                        "$unionWith": {
-                            "coll": "myntra-items",
-                            "pipeline": [
-                                {
-                                    "$search": {
-                                        "index": text_search_index_name,
-                                        "text": {
-                                            "query": text_search,
-                                            "path": "name",
-                                        },
-                                        # "phrase": {"query": text_search, "path": "name"},
-                                    }
-                                },
-                                {
-                                    # TODO - implement $match using pre-filters instead of a post-$search step
-                                    "$match": {
-                                        "price": {"$gte": min_price, "$lte": max_price},
-                                        "rating": {"$gte": min_rating},
-                                        "category": {"$in": synthetic_categories},
-                                        "color": {"$in": colors},
-                                        "season": {"$in": seasons},
-                                    }
-                                },
-                                {"$limit": 20},
-                                {"$group": {"_id": None, "docs": {"$push": "$$ROOT"}}},
-                                {
-                                    "$unwind": {
-                                        "path": "$docs",
-                                        "includeArrayIndex": "rank",
-                                    }
-                                },
-                                {
-                                    "$addFields": {
-                                        "fts_score": {
-                                            "$divide": [
-                                                1.0,
-                                                {
-                                                    "$add": [
-                                                        "$rank",
-                                                        full_text_penalty,
-                                                        1,
-                                                    ]
-                                                },
-                                            ]
-                                        }
-                                    }
-                                },
-                                {
-                                    "$project": {
-                                        "fts_score": 1,
-                                        "_id": "$docs._id",
-                                        "name": "$docs.name",
-                                        "img": "$docs.img",
-                                    }
-                                },
-                            ],
-                        }
-                    },
-                    {
-                        "$group": {
-                            "_id": "$name",
-                            "img": {"$first": "$img"},
-                            "vs_score": {"$max": "$vs_score"},
-                            "fts_score": {"$max": "$fts_score"},
-                        }
-                    },
-                    {
-                        "$project": {
-                            "_id": 1,
-                            "img": 1,
-                            "vs_score": {"$ifNull": ["$vs_score", 0]},
-                            "fts_score": {"$ifNull": ["$fts_score", 0]},
-                        }
-                    },
-                    {
-                        "$project": {
-                            "score": {"$add": ["$fts_score", "$vs_score"]},
-                            "name": "$_id",
-                            "img": 1,
-                            "vs_score": 1,
-                            "fts_score": 1,
-                        }
-                    },
-                    {"$sort": {"score": -1}},
-                    {"$limit": n},
-                ]
-            )
-        else:
-            records = collection.aggregate(
-                [
-                    {
-                        "$match": {
+
+            pipeline = [
+                {
+                    "$vectorSearch": {
+                        "index": vector_search_index_name,
+                        "path": vector_search_path,
+                        "queryVector": embedding.tolist(),
+                        "numCandidates": 100,
+                        "limit": 20,
+                        "filter": {
                             "price": {"$gte": min_price, "$lte": max_price},
                             "rating": {"$gte": min_rating},
                             "category": {"$in": synthetic_categories},
                             "color": {"$in": colors},
                             "season": {"$in": seasons},
+                        },
+                    }
+                },
+                {"$group": {"_id": None, "docs": {"$push": "$$ROOT"}}},
+                {"$unwind": {"path": "$docs", "includeArrayIndex": "rank"}},
+                {
+                    "$addFields": {
+                        "vs_score": {
+                            "$divide": [1.0, {"$add": ["$rank", vector_penalty, 1]}]
                         }
-                    },
-                    {"$limit": n},
-                ]
-            )
+                    }
+                },
+                {
+                    "$project": {
+                        "vs_score": 1,
+                        "_id": "$docs._id",
+                        "name": "$docs.name",
+                        "img": "$docs.img",
+                    }
+                },
+                {
+                    "$unionWith": {
+                        "coll": "myntra-items",
+                        "pipeline": [
+                            {
+                                "$search": {
+                                    "index": text_search_index_name,
+                                    "text": {
+                                        "query": text_search,
+                                        "path": "name",
+                                    },
+                                    # "phrase": {"query": text_search, "path": "name"},
+                                }
+                            },
+                            {
+                                # TODO - implement $match using pre-filters instead of a post-$search step
+                                "$match": {
+                                    "price": {"$gte": min_price, "$lte": max_price},
+                                    "rating": {"$gte": min_rating},
+                                    "category": {"$in": synthetic_categories},
+                                    "color": {"$in": colors},
+                                    "season": {"$in": seasons},
+                                }
+                            },
+                            {"$limit": 20},
+                            {"$group": {"_id": None, "docs": {"$push": "$$ROOT"}}},
+                            {
+                                "$unwind": {
+                                    "path": "$docs",
+                                    "includeArrayIndex": "rank",
+                                }
+                            },
+                            {
+                                "$addFields": {
+                                    "fts_score": {
+                                        "$divide": [
+                                            1.0,
+                                            {
+                                                "$add": [
+                                                    "$rank",
+                                                    full_text_penalty,
+                                                    1,
+                                                ]
+                                            },
+                                        ]
+                                    }
+                                }
+                            },
+                            {
+                                "$project": {
+                                    "fts_score": 1,
+                                    "_id": "$docs._id",
+                                    "name": "$docs.name",
+                                    "img": "$docs.img",
+                                }
+                            },
+                        ],
+                    }
+                },
+                {
+                    "$group": {
+                        "_id": "$name",
+                        "img": {"$first": "$img"},
+                        "vs_score": {"$max": "$vs_score"},
+                        "fts_score": {"$max": "$fts_score"},
+                    }
+                },
+                {
+                    "$project": {
+                        "_id": 1,
+                        "img": 1,
+                        "vs_score": {"$ifNull": ["$vs_score", 0]},
+                        "fts_score": {"$ifNull": ["$fts_score", 0]},
+                    }
+                },
+                {
+                    "$project": {
+                        "score": {"$add": ["$fts_score", "$vs_score"]},
+                        "name": "$_id",
+                        "img": 1,
+                        "vs_score": 1,
+                        "fts_score": 1,
+                    }
+                },
+                # filter on score threshold
+                {
+                    "$match": {
+                        "score": {"$gt": score_threshold},
+                    }
+                },
+                {"$sort": {"score": -1}},
+                {"$limit": n},
+            ]
+        else:
+            pipeline = [
+                {
+                    "$match": {
+                        "price": {"$gte": min_price, "$lte": max_price},
+                        "rating": {"$gte": min_rating},
+                        "category": {"$in": synthetic_categories},
+                        "color": {"$in": colors},
+                        "season": {"$in": seasons},
+                    }
+                },
+                {"$limit": n},
+            ]
 
-        return [
-            (record["img"].split(";")[-1].strip(), record["name"])
-            async for record in records
+        records = collection.aggregate(pipeline)
+        logger.debug(f"Running pipeline: {pipeline}")
+        records = [record async for record in records]
+        logger.debug([(record["name"], record["score"]) for record in records])
+        results = [
+            (record["img"].split(";")[-1].strip(), record["name"]) for record in records
         ]
+        num_results = len(results)
+
+        logger.debug(f"Found {num_results=} results")
+        return results
 
 
 fastapi = FastAPI()
